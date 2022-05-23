b6f563e2fc186efaf98ad04328b714191147b14e
My2Labs <sking171@ford.com>
2022-05-23 15:58:07 -0500

    Work exercise.

--- App.java
+++ App.java
@@ -1,6 +1,14 @@
 class App {
-  public static void main(String args[]){
+  public static void main(String args[]) {
     String message = "Hello, world!";
-    System.out.println(message);
+    String vin = "0102030";
+    String make = "Ford F-150";
+    String color = "Black";
+    String towing = "Towing package included";
+    String quality = "A";
+    String odometer = "1,400 miles";
+    String price = "$48,000";
+
+    System.out.println(message + vin + make + color + towing + quality + odometer + price);
   }
 }
